484d6bb7a5cb702763e6956e0730dc3835bc301a
IterIV <ngminhduc1078@gmail.com>
2022-02-25 22:48:51 +0700

    fix name of TopNav -> Top

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../../components/Sidebar";
-import TopNav from "../../components/TopNav";
+import TopNav from "../../components/Top";
 const MainLayout = () => {
   const navigate = useNavigate();
   const { user } = useSelector((rootReducer) => rootReducer.AuthReducer);
